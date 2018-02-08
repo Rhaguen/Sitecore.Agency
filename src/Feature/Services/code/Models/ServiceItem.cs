d13c521af045c3ae2e941625417a1b43268f62ed
Eduardo Getassi da Rosa <rhaguen@gmail.com>
2018-02-08 14:14:29 -0200

    Templates and Models for Services

---
+++ src/Feature/Services/code/Models/ServiceItem.cs
@@ -0,0 +1,15 @@
+﻿using Agency.Foundation.Orm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Agency.Feature.Services.Models
+{
+    public class ServiceItem : SitecoreItem
+    {
+        public string IconType { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ src/Feature/Services/code/Models/ServicesContainer.cs
@@ -0,0 +1,12 @@
+﻿using Agency.Foundation.Orm.Models;
+using System.Collections.Generic;
+
+namespace Agency.Feature.Services.Models
+{
+    public class ServicesContainer : SitecoreItem
+    {
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public IEnumerable<ServiceItem> Children { get; set; }
+    }
+}
