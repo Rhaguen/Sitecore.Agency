7b3f74e1eada8ffb71f732045b26e0564b8cb599
Eduardo Getassi da Rosa <rhaguen@gmail.com>
2018-02-08 14:21:23 -0200

    Installing Microsoft.Asp.Net.MVC

---
+++ src/Feature/Services/code/Controllers/ServicesController.cs
@@ -0,0 +1,18 @@
+﻿using Glass.Mapper.Sc;
+using Glass.Mapper.Sc.Web.Mvc;
+using System.ComponentModel.Design;
+using System.Web.Mvc;
+
+namespace Agency.Feature.Services.Controllers
+{
+    public class ServicesController : GlassController
+    {
+        public ActionResult Service()
+        {
+            var context = new SitecoreContext();
+            var mainMenu = context.GetItem<ServiceContainer>("/sitecore/content/Agency/Data/Services Container");
+
+            return View(mainMenu);
+        }
+    }
+}
--- src/Feature/Services/code/Models/ServiceItem.cs
+++ src/Feature/Services/code/Models/ServiceItem.cs
@@ -1,8 +1,4 @@
 ﻿using Agency.Foundation.Orm.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace Agency.Feature.Services.Models
 {
