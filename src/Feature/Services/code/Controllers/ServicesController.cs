f10c2ae43ab232e7685b34b72f3e4247475c0e0c
Eduardo Getassi da Rosa <rhaguen@gmail.com>
2018-02-08 14:44:14 -0200

    Rendering and Add rendering to page layout

--- src/Feature/Services/code/Controllers/ServicesController.cs
+++ src/Feature/Services/code/Controllers/ServicesController.cs
@@ -1,18 +1,18 @@
-﻿using Glass.Mapper.Sc;
+﻿using Agency.Feature.Services.Models;
+using Glass.Mapper.Sc;
 using Glass.Mapper.Sc.Web.Mvc;
-using System.ComponentModel.Design;
 using System.Web.Mvc;
 
 namespace Agency.Feature.Services.Controllers
 {
     public class ServicesController : GlassController
     {
-        public ActionResult Service()
+        public ActionResult ServiceContainer()
         {
             var context = new SitecoreContext();
-            var mainMenu = context.GetItem<ServiceContainer>("/sitecore/content/Agency/Data/Services Container");
+            var serviceContainer = context.GetItem<ServicesContainer>("/sitecore/content/Agency/Data/Services Container");
 
-            return View(mainMenu);
+            return View(serviceContainer);
         }
     }
 }
