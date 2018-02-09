15d0acae00c07dff7bac132fd452bce089d697c5
Eduardo Getassi da Rosa <rhaguen@gmail.com>
2018-02-09 11:57:09 -0200

    Portfolio view

---
+++ src/Feature/Portfolio/code/Models/PortfolioItem.cs
@@ -0,0 +1,18 @@
+﻿using Agency.Foundation.Orm.Models;
+using Glass.Mapper.Sc.Fields;
+using System;
+
+namespace Agency.Feature.Portfolio.Models
+{
+    public class PortfolioItem : SitecoreItem
+    {
+        public string Client { get; set; }
+        public string Category { get; set; }
+        public Image Image { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public Link Thumbnail { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectTitle { get; set; }
+    }
+}
